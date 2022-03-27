8178dc1bbf4086d870e12bd873275f14704f9555
System Administrator <root@yang-giugs-MacBook-Air.local>
2022-03-27 14:24:54 +0900

    Feat: 메인 및 post 페이지 레이아웃 수정

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,15 @@
 import Link from 'next/link'
+import { BLOG_OWNER } from '../lib/constants'
 
 const Header = () => {
-  return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/">
-        <a className="hover:underline">Blog</a>
-      </Link>
-      .
-    </h2>
-  )
+    return (
+        <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+            <Link href="/">
+                <a className="hover:underline">{BLOG_OWNER}</a>
+            </Link>
+            .
+        </h2>
+    )
 }
 
 export default Header
--- components/layout.tsx
+++ components/layout.tsx
@@ -3,21 +3,19 @@ import Footer from './footer'
 import Meta from './meta'
 
 type Props = {
-  preview?: boolean
-  children: React.ReactNode
+    preview?: boolean
+    children: React.ReactNode
 }
 
 const Layout = ({ preview, children }: Props) => {
-  return (
-    <>
-      <Meta />
-      <div className="min-h-screen">
-        <Alert preview={preview} />
-        <main>{children}</main>
-      </div>
-      <Footer />
-    </>
-  )
+    return (
+        <>
+            <Meta />
+            <div className="min-h-screen">
+                <main>{children}</main>
+            </div>
+        </>
+    )
 }
 
 export default Layout
